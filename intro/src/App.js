39d9c89c14881f8f5662ef6f61587149e45774d5
mehmetuar <mehmetuar@hotmail.com>
2025-06-07 13:14:01 +0300

    intro klasörünü submodule olmadan ekledim

---
+++ intro/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import { Container, Row, Col } from 'reactstrap'
+
+
+export default class App extends Component {
+
+  state = { currentCategory: "", products: [],cart:[] };
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName })
+    this.getProducts(category.id);
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ products: data }));
+  };
+
+  addToCart=(product) => {
+    let newCart = this.state.cart;
+   var addedItem = newCart.find(c=>c.product.id === product.id); 
+   if(addedItem){
+    addedItem.quantity+=1;
+   }
+   else{
+    newCart.push({product:product, quantity:1});
+   }
+   
+   this.setState({cart: newCart})
+  }
+
+
+  render() {
+    let ProductInfo = { title: "Product List" }
+    let CategoryInfo = { title: "Category List" }
+    return (
+      <div >
+        <Container>
+       
+            <Navi cart={this.state.cart} />
+        
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={CategoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                products={this.state.products}
+                addToCart = {this.addToCart}
+                currentCategory={this.state.currentCategory}
+                info={ProductInfo} />
+            </Col>
+
+          </Row>
+        </Container>
+
+
+
+      </div>
+    );
+  }
+}
---
+++ intro/src/Navi.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+} from "reactstrap";
+import CartSummary from "./CartSummary";
+
+
+export default class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Northwind App</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            
+              <CartSummary
+                removeFromCart={this.props.removeFromCart}
+                cart={this.props.cart}
+              />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
